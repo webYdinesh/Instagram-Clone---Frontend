a33d9370f914f9ca1e20088d8f672681fa37fb6d
= <=>
2023-01-31 12:54:35 +0530

    set loading bar

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -104,7 +104,7 @@ const Profile = () => {
                   savedPost: userProfile?.savedPost,
               });
         setisMyProfile(true);
-    }, [userProfile, params]);
+    }, [userProfile]);
 
     //followUnfollowHandler
     const followUnfollowHandler = async () => {
